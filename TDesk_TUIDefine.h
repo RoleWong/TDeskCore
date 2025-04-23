ede604e9156f035c310bb38056dae3fb14703a88
RoleWong <814209329@qq.com>
2025-04-23 10:34:46 +0800

    Version: 2.9.0

--- TDesk_TUIDefine.h
+++ TDesk_TUIDefine.h
@@ -59,6 +59,7 @@
 #define BussinessID_Src_CustomerService_Card @"22"
 #define BussinessID_Src_CustomerService_EvaluationRule @"23"
 #define BussinessID_Src_CustomerService_EvaluationTrigger @"24"
+#define BussinessID_Src_CustomerService_Agent_Status @"26"
 #define BussinessID_Src_CustomerService_Bot_Welcome_Clarify @"29" // Supported in 8.1 and later
 #define BussinessID_Src_CustomerService_Bot_Rich_Text @"30" // Supported in 8.1 and later
 #define BussinessID_Src_CustomerService_Bot_Stream_Text @"31" // Supported in 8.1 and later
